63271f3c6e200b3308059271513cd59cef24602a
Jonathan <jdguayllas@utpl.edu.ec>
2024-01-04 17:42:26 -0500

    problema 1 resuelto

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,59 @@ public class Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        // TODO code application logic here}
+        double area;
+        System.out.println("Ingrese 1 para el area de un cuadrado, ingrese 2"
+                + " para el area de un triángulo, ingrese 3 para el area de un"
+                + "rectangulo");
+        int tipo = entrada.nextInt();
+        if (tipo == 1) {
+            area = obtenerAreaCuadrado();
+        } else {
+            if (tipo == 2) {
+                area = obtenerAreaTriangulo();
+            } else {
+                if (tipo == 3) {
+                    area = obtenerAreaRectangulo();
+                } else {
+                    System.out.println("opción incorrecta");
+                }
+            }
+
+        }
+
     }
-    
+    public static double obtenerAreaCuadrado(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el lado del cuadrado");
+        double suma;
+        suma = entrada.nextDouble();
+        suma = suma + suma + suma + suma;
+        return suma;
+    } 
+    public static double obtenerAreaTriangulo(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la base del triangulo");
+        double base;
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del triangulo");
+        double altura;
+        altura = entrada.nextDouble();
+        double operacion;
+        operacion = (base * altura) / 2;
+        return operacion;
+    }
+    public static double obtenerAreaRectangulo(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la base del rectangulo");
+        double base;
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del rectangulo");
+        double altura;
+        altura = entrada.nextDouble();
+        double operacion;
+        operacion = (base * altura);
+        return operacion;
+    }
 }
